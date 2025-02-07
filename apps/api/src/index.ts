38abb5cbb00e9fe64aaf611455405c8d825dc0c3
Christophe Kafrouni <44137603+ckafrouni@users.noreply.github.com>
2025-02-07 01:24:17 +0100

    change api build folder

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,15 +4,13 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello Hono!" });
+  return c.text("Hello Hono!");
 });
 
 const port = 3032;
-const hostname = "0.0.0.0";
-console.log(`Server is running on http://${hostname}:${port}`);
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
   port,
-  hostname,
 });
